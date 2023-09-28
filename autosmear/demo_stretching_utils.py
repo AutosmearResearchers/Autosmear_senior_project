63373465b6e6ebc0d149969a309629ed5bc7c946
Chitrakorn Phaetlakfa <98763553+CCKP14780@users.noreply.github.com>
2023-09-28 14:29:52 +0700

    merge three sub-features together
    These three features are:
    i. Auto Stretch Smear
    i. Auto Stretch Smear

--- autosmear/demo_stretching_utils.py
+++ autosmear/demo_stretching_utils.py
@@ -12,7 +12,10 @@ def get_values(
     start_ctrl="",
     end_ctrl="",
     command_option=1,
-    ctrl_hierarchy=[]):
+    ctrl_hierarchy=[],
+    interval = 1,
+    custom_frame = 1,
+    smear_option = 1):
     """
     main function for proceeding stretching feature
 
@@ -25,6 +28,7 @@ def get_values(
         start_ctrl (str): start joint controller
         end_ctrl (str): end joint controller
         command_option (int): the chosen option number for different operations (Based on ...)
+        smear_option (int): the chosen option for the type of smear that must be generated
     """
     
     #todo check if user choose based on attribute or controller
@@ -32,7 +36,12 @@ def get_values(
         #! using function to get the hierarchy of the ctrl
         # ctrl_hierarchy = get_ctrl_hierarchy(start_ctrl,end_ctrl)
         #! using function to calculate the velocity of the obj from start_frame to end_frame
-        smear_frame = calculate_velocity(start_frame,end_frame,ctrl_hierarchy)
+        if smear_option == 1:
+            smear_frame = calculate_velocity(start_frame,end_frame,ctrl_hierarchy)  #auto smear
+        elif smear_option == 2:
+            smear_frame = calculate_interval_smear(start_frame,end_frame,interval)  #interval smear
+        else:
+             smear_frame = calculate_custom_smear(custom_frame)     #custom smear
         #! using function to keyframe the stretch smear effect (by ctrl)
         stretch_ctrl(start_frame,end_frame,smear_frame,ctrl_hierarchy,multiplier)
 
@@ -42,7 +51,12 @@ def get_values(
         #! using function to query [[range],default] of the selected attribute
         attribute_value_list = find_attribute_range(raw_squash_attribute,master_squash_attribute)
         #! using function to calculate the velocity of the obj from start_frame to end_frame
-        smear_frame = calculate_velocity(start_frame=start_frame,end_frame=end_frame,ctrl_hierarchy_list=tmp_attr_list)
+        if smear_option == 1:
+            smear_frame = calculate_velocity(start_frame,end_frame,ctrl_hierarchy)  #auto smear
+        elif smear_option == 2:
+            smear_frame = calculate_interval_smear(start_frame,end_frame,interval)  #interval smear
+        else:
+             smear_frame = calculate_custom_smear(custom_frame)     #custom smear
         #! using function to keyframe the stretch smear effect (by attr)
         stretch_attribute(smear_frame,end_frame,master_squash_attribute,raw_squash_attribute,attribute_value_list,multiplier)
 
@@ -58,14 +72,14 @@ def find_attribute_range(raw_squash_attribute = "",master_squash_attribute = "")
     attribute_default = cmds.attributeQuery(raw_squash_attribute,node = master_squash_attribute,listDefault = True)
     return([attribute_max[0], attribute_default[0]])
 
-def stretch_attribute(smear_frame=1,end_frame = 1,master_squash_attribute="",raw_squash_attribute="",attribute_value_list=[],multiplier=1.0):
+def stretch_attribute(smear_frames=[],end_frame = 1,master_squash_attribute="",raw_squash_attribute="",attribute_value_list=[],multiplier=1.0):
     """
     keyframing function
     this function is developed to support calculations in both cases;
     Based on attribute and Based on hierarchical node (controller)
 
     Args:
-        smear_frame (int): calculated frame that smear will be created on
+        smear_frames (list): list of calculated frames that smear will be created on
         end_frame (int): end frame of smear
         master_squash_attribute (str): node squash/stretch attribute
         raw_squash_attribute (str): parent node of squash/stretch attribute (controller)
@@ -81,20 +95,21 @@ def stretch_attribute(smear_frame=1,end_frame = 1,master_squash_attribute="",raw
     else:
          multiplier = 1
     
-    #! keyframe the frame before smear_frame
-    cmds.currentTime(smear_frame - 1)
-    cmds.setAttr(attribute,original_stretch_value)
-    cmds.setKeyframe(attribute,breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
+    for smear_frame in smear_frames:
+        #! keyframe the frame before smear_frame
+        cmds.currentTime(smear_frame - 1)
+        cmds.setAttr(attribute,original_stretch_value)
+        cmds.setKeyframe(attribute,breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
 
-    #! keyframe the smear_frame aka. Stretch the rig
-    cmds.currentTime(smear_frame)
-    cmds.setAttr(attribute,max_stretch_value)
-    cmds.setKeyframe(attribute,breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
+        #! keyframe the smear_frame aka. Stretch the rig
+        cmds.currentTime(smear_frame)
+        cmds.setAttr(attribute,max_stretch_value)
+        cmds.setKeyframe(attribute,breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
 
-    #! keyframe the end_frame
-    cmds.currentTime(end_frame)
-    cmds.setAttr(attribute,original_stretch_value)
-    cmds.setKeyframe(attribute,breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
+        #! keyframe the end_frame
+        cmds.currentTime(end_frame)
+        cmds.setAttr(attribute,original_stretch_value)
+        cmds.setKeyframe(attribute,breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
 
 def get_ctrl_hierarchy(start_ctrl = "",end_ctrl = ""):
     """
@@ -172,50 +187,76 @@ def calculate_velocity(start_frame=1,end_frame=1,ctrl_hierarchy=[]):
             max_a = a
             smear_frame = vi + start_frame
     
-    return(smear_frame)
+    return [smear_frame]
 
-def stretch_ctrl(start_frame=1,end_frame=1,smear_frame = 1,ctrl_hierarchy = [],multiplier = 1.0):    
+def calculate_interval_smear(start_frame=1,end_frame=1,interval=1):
     """
-    calculating velocity
+    calculating an interval smear
 
     Args:
-        smear_frame (int): calculated frame that smear will be created on
+        start_frame (int): start keyframe
+        end_frame (int): end keyframe
+        interval (int): interval of the smear
+    """
+    smear_frames = []
+    for each_frame in range(start_frame,end_frame+1,interval):
+        smear_frames.append(each_frame)
+
+    return smear_frames
+
+def calculate_custom_smear(custom_frame=1):
+     """
+    returns the custom_smear_frames
+
+    Args:
+        custom_frame (int): custom smear frame
+    """  
+     return [custom_frame]
+
+def stretch_ctrl(start_frame=1,end_frame=1,smear_frames = [],ctrl_hierarchy = [],multiplier = 1.0):    
+    """
+    stretch_ctrl
+
+    Args:
+        smear_frames (list): calculated frames that smear will be created on
         ctrl_hierarchy (list): list of hierarchical controller
         multiplier (float): value used to increase the strength of the smear
     """
     number_of_ctrl = len(ctrl_hierarchy)
     locator_list = []
     used_ctrl=[]
+    for smear_frame in smear_frames:
+        for number in range(1,number_of_ctrl):
+            #todo keyframe start/end frame first!
+            cmds.currentTime(start_frame)
+            cmds.setKeyframe(ctrl_hierarchy[number],breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
+            cmds.currentTime(end_frame)
+            cmds.setKeyframe(ctrl_hierarchy[number],breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
 
-    for number in range(1,number_of_ctrl):
-        #todo keyframe start/end frame first!
-        cmds.currentTime(start_frame)
-        cmds.setKeyframe(ctrl_hierarchy[number],breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
-        cmds.currentTime(end_frame)
-        cmds.setKeyframe(ctrl_hierarchy[number],breakdown = False, preserveCurveShape = False, hierarchy = "None",controlPoints= False, shape = False)
-
-        #todo create ctrl on the smear_frame
-        cmds.currentTime(smear_frame)
-        ctrl_transaltion = cmds.xform(ctrl_hierarchy[number],query=True,translation=True,worldSpace=True)
+            #todo create ctrl on the smear_frame
+            
+            cmds.currentTime(smear_frame)
+            ctrl_transaltion = cmds.xform(ctrl_hierarchy[number],query=True,translation=True,worldSpace=True)
         
-        #todo create locator on the location
-        if cmds.objExists("{ctrlHie}_autoSmearTool_LOC".format(ctrlHie = ctrl_hierarchy[number])) is False:    
-            loc = cmds.spaceLocator(name = "{ctrlHie}_autoSmearTool_LOC".format(ctrlHie = ctrl_hierarchy[number]))
-            locator_list.append(loc[0])
-            #!get and set the locator value
-            cmds.xform(loc,translation=ctrl_transaltion,worldSpace=True)
-            loc_translate = cmds.xform(loc,query=True,translation=True,worldSpace=True)
-            #!snap the ctrl to locator
-            cmds.xform(ctrl_hierarchy[number],translation=loc_translate,worldSpace=True)
+            #todo create locator on the location
+            if cmds.objExists("{ctrlHie}_{frame}_autoSmearTool_LOC".format(frame=smear_frame,ctrlHie = ctrl_hierarchy[number])) is False:    
+                loc = cmds.spaceLocator(name = "{ctrlHie}_{frame}_autoSmearTool_LOC".format(frame=smear_frame,ctrlHie = ctrl_hierarchy[number]))
+                locator_list.append(loc[0])
+                #!get and set the locator value
+                cmds.xform(loc,translation=ctrl_transaltion,worldSpace=True)
+                loc_translate = cmds.xform(loc,query=True,translation=True,worldSpace=True)
+                #!snap the ctrl to locator
+                cmds.xform(ctrl_hierarchy[number],translation=loc_translate,worldSpace=True)
 
-            #todo snap the ctrl of the next frame to the created loc
-            used_frame = smear_frame+1
-            cmds.currentTime(used_frame)
-            cmds.xform(ctrl_hierarchy[number],translation=loc_translate,worldSpace=True)
-            used_ctrl.append(ctrl_hierarchy[number])
+                #todo snap the ctrl of the next frame to the created loc
+                used_frame = smear_frame+1
+                cmds.currentTime(used_frame)
+                cmds.xform(ctrl_hierarchy[number],translation=loc_translate,worldSpace=True)
+                used_ctrl.append(ctrl_hierarchy[number])
     
-    #todo group all the locators created after the iteration is complete
-    loc_group = cmds.group(locator_list,name = "{ctrlHie}_autoSmearTool_LOC_grp".format(ctrlHie = ctrl_hierarchy[number]))
+        #todo group all the locators created after the iteration is complete
+        loc_group = cmds.group(locator_list,name = "{ctrlHie}_{frame}_autoSmearTool_LOC_grp".format(frame=smear_frame,ctrlHie = ctrl_hierarchy[number]))
+        locator_list = []
 
     #! create history dict and record history of smear
     order_num = 1
@@ -229,6 +270,4 @@ def stretch_ctrl(start_frame=1,end_frame=1,smear_frame = 1,ctrl_hierarchy = [],m
 
     cmds.addAttr("smear_history_grp", ln=attr_naming, dt="string")
     cmds.setAttr("smear_history_grp.{attr_name}".format(attr_name = attr_naming), history_dict, type="string", lock = True)
-
-#! keyframe the Charlizard
 
